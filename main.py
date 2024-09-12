a33f0ef56ff271947565f1b2009ba2d6c70653da
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-12 12:33:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+if __name__ == '__main__':
+    with app.test_request_context():
+        html_content = app.dispatch_request()
+    
+    with open('output.html', 'w') as f:
+        f.write(html_content)
