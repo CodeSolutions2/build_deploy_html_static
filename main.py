563ae7da85a80e80912c3b966f58f8f5b4769c31
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-13 18:45:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,91 +1,33 @@
-# -----------------------------
-
 def display_figure(json_str):
   import pandas as pd
-
-  # --------------------------------
-  
-  # Read json data
-  # data = pd.read_json(json_str)
-  # x_axis_variable = 'petal_width' # sepal_length, sepal_width, petal_length, petal_width
-  # y_axis_variable = 'species'
   
   data = pd.DataFrame({'a': list('CCCDDDEEE'), 'b': [2, 7, 4, 1, 2, 6, 8, 4, 7]});
   x_axis_variable = 'a'
   y_axis_variable = 'b'
 
-  # Read csv from runner
-  # data = pd.DataFrame("/home/runner/work/static_HTML/static_HTML/data.csv");
-  # x_axis_variable = 'petal_width' # sepal_length, sepal_width, petal_length, petal_width
-  # y_axis_variable = 'species'
-  # DataFrame error
-
-  # Process string data
-  # json_str.split();
-  # --------------------------------
-  
   # Make the chart
   import altair as alt
   chart = alt.Chart(data).mark_point().encode(x=x_axis_variable, y=y_axis_variable).interactive()
 
-  return [chart.to_html(), json_str];
+  return chart.to_html();
   
 # -----------------------------
-
 
 # -----------------------------
 # Main
 # -----------------------------
-import sys
-json_str = sys.argv[1]
-
 title = "Display data"
 
-# -----------------------------
-
-# Test 0: Output python computation to HTML
-# data = 5+5;
-
-# Test 1: Output function output to HTML
-# data = run_process();
-
-# Test 2: Output figure to HTML
-
-# Could do selineum and webscrape the html input
-
-
-# Could read url_content from a file using GET
-[html_chart, url_data] = display_figure(json_str);
+html_chart = display_figure(json_str);
   
 print(f'''<!DOCTYPE html>
 <html>
 <head></head>
 <body>
 <h1>{title}</h1>
-<h2>Input a dataset url</h2>
-<p>For example, enter https://raw.githubusercontent.com/CodeSolutions2/static_HTML/main/iris.csv.</p>
-<input type="text" name="data_url" id="data_url" value="" style="display:block;">
-<button id="refresh_plot" onclick="refresh_plot()" style="display:block">Refresh plot</button>
 <div>{html_chart}</div>
-<h2>Data from url</h2>
-<div>{url_data}</div>
-<script type="module" redirect="follow" mode="no-cors">import {{ run_backend_process, GET_fileDownloadUrl_and_sha }} from "https://codesolutions2.github.io/static_HTML/library_to_run_GitHub_Actions.js";
-module.run_backend_process = run_backend_process;
-module1.GET_fileDownloadUrl_and_sha = GET_fileDownloadUrl_and_sha;
 </script>
 <script>
-const module = {{}};
-async function refresh_plot() {{
-var RepoAobj = {{}};
-RepoAobj.repoOwner = 'CodeSolutions2';
-RepoAobj.repoA_name = 'static_HTML';
-RepoAobj.foldername = '';
-RepoAobj.filename = "launch.txt";
-const random_text = Math.random().toString(16).slice(2);
-RepoAobj.input_text = document.getElementById("data_url").value+"|"+RepoAobj.repoA_name+"_"+random_text;
-RepoAobj.repoB_name = 'static_HTML';
-await module.run_backend_process(RepoAobj);
-}}
 </script>
 </body>
 </html>''')
