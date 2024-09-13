4af1687576d7d97514ff2a370bb01b7fcff5b640
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-13 12:29:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 # def run_process():
 #   return 4+4;
 
-
-if __name__ == '__main__':
-  title = "Display data"
-  # data = run_process();
-  data = 4+4;
+title = "Display data"
+# data = run_process();
+data = 4+4;
   
-  print(f'''<!DOCTYPE html>
+print(f'''<!DOCTYPE html>
 <html>
 <head></head>
 <body><h1>{title}</h1><p>Output: {data}</p><script></script>
