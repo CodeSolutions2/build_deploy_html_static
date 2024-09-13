cdf9ad6023684208093596cf0956aefe33378571
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-13 10:22:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,5 @@ def hello():
 if __name__ == '__main__':
     with app.test_request_context():
         html_content = app.dispatch_request()
-    
-    with open('index.html', 'w') as f:
-        f.write(html_content)
+
+    print(html_content)
