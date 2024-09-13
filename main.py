0f43e897753f331990c371b5caefb493bca97b2f
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-13 12:20:41 +0200

    Update and rename main1.py to main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+def run_process():
+  
+  return 4+4;
+
+
+if __name__ == '__main__':
+  title = "Display data"
+  data = run_process();
+  
+  print(f'''<!DOCTYPE html>
+<html>
+<head></head>
+<body><h1>{title}</h1><p>Output: {data}</p><script></script>
+</body>
+</html>''')
--- main1.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-  print('''<!DOCTYPE html>
-<html>
-<head></head>
-<body><h1>Hello, World!</h1><script></script>
-</body>
-</html>''')
