ed5d2c1d45e11f1f6cb6e53112bfa45f3592da16
Jamilah Foucher <91731082+j622amilah@users.noreply.github.com>
2024-09-25 14:47:48 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,39 @@ def display_figure():
 title = "Display data"
 
 html_chart = display_figure();
-  
-print(f'''
-{html_chart}
-''')
+
+# Way 0: Recreates the HTML automatically, but less good if you want to control what parts go where in the HTML document
+# print(f'''
+# {html_chart}
+# ''')
+
+
+# Way 1: Control parts of the html
+import regex
+
+# Remove all newline characters
+re_exp = "\n"
+html_chart = regex.sub(re_exp, "", html_chart)
+# print('html_chart: ', html_chart)
+
+# Replace 2 or more consecutive spaces with one space
+re_exp = "\s+"
+html_chart1 = regex.sub(re_exp, " ", html_chart)
+print('html_chart1: ', html_chart1)
+
+# Assign the correct code to the desired HTML sections
+altair_body_output = text[0][0] + '' + text[1][0];
+altair_script_output = text[2][0];
+
+print(f'''<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+<h1>{title}</h1>
+<div id=altair_body_output>{altair_body_output}</div>
+</script>
+{altair_script_output}
+<script>
+</script>
+</body>
+</html>''')
